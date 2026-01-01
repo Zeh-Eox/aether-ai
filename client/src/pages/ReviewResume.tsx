05bc5e943fc8e95891e242498f739910a2aebf2d
Zeh-Eox <arnolddevmail@gmail.com>
2026-01-01 16:22:39 +0000

    Review Resume func. implemented in the Frontend

--- client/src/pages/ReviewResume.tsx
+++ client/src/pages/ReviewResume.tsx
@@ -1,14 +1,55 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { FileText, Sparkles } from "lucide-react";
+import axios from "axios";
+import { useAuth } from "@clerk/clerk-react";
+import toast from "react-hot-toast";
+import Markdown from "react-markdown";
+
+axios.defaults.baseURL =
+  import.meta.env.VITE_API_URL || "http://localhost:3000/api/v1";
 
 const ReviewResume: React.FunctionComponent = () => {
   const [file, setFile] = React.useState<File | null>(null);
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [analysisResult, setAnalysisResult] = React.useState<string>("");
 
-  console.log(file);
+  const { getToken } = useAuth();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    try {
+      setLoading(true);
+      if (!file) {
+        toast.error("Please upload a resume file");
+        setLoading(false);
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append("resume", file);
+
+      const { data } = await axios.post("/ai/review-resume", formData, {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      if (data.success) {
+        setAnalysisResult(data.content);
+      } else {
+        toast.error(data.message || "Failed to analyze resume");
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        toast.error(error.response?.data?.message || "API error");
+      } else {
+        toast.error("Unexpected error occurred");
+      }
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -42,8 +83,18 @@ const ReviewResume: React.FunctionComponent = () => {
           className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00DA83] to-[#009BB3] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer"
           whileHover={{ scale: 1.02 }}
           whileTap={{ scale: 0.95 }}
+          disabled={loading}
         >
-          <FileText className="w-5" /> Review Resume
+          {loading ? (
+            <>
+              <span className="w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin"></span>
+              Reviewing Resume...
+            </>
+          ) : (
+            <>
+              <FileText className="w-5" /> Review Resume
+            </>
+          )}
         </motion.button>
       </form>
       <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-[520px] max-h-[600px]">
@@ -51,14 +102,23 @@ const ReviewResume: React.FunctionComponent = () => {
           <FileText className="w-5 h-5 text-[#00DA83]" />
           <h1 className="text-xl font-semibold">Resume Analysis Result</h1>
         </div>
-        <div className="flex flex-1 justify-center items-center">
-          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
-            <FileText className="w-9 h-9" />
-            <p>
-              Upload a resume and click <b>Review Resume</b> to get started
-            </p>
+
+        {!analysisResult ? (
+          <div className="flex flex-1 justify-center items-center">
+            <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+              <FileText className="w-9 h-9" />
+              <p>
+                Upload a resume and click <b>Review Resume</b> to get started
+              </p>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="mt-3 h-full overflow-y-scroll text-sm text-slate-600">
+            <div className="reset-tw">
+              <Markdown>{analysisResult}</Markdown>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
