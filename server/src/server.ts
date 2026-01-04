abb4fa01d65859a31d0fcb94e02b1c3e8d974f1f
Zeh-Eox <arnolddevmail@gmail.com>
2026-01-04 23:10:02 +0000

    Minor Vercel prod bug fixed

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,8 @@ import userRouter from "./routes/userRoutes";
 
 const app = express();
 
+app.set("trust proxy", 1);
+
 connectCloudinary();
 
 app.use(cors());
--- server/src/utils/pdfReader.ts
+++ server/src/utils/pdfReader.ts
@@ -1,17 +1,7 @@
 export const extractTextFromPDF = async (buffer: Buffer): Promise<string> => {
-  const pdfjs = await import("pdfjs-dist/legacy/build/pdf.mjs");
-  const pdfjsLib = pdfjs.default ?? pdfjs;
-
-  const data = new Uint8Array(buffer);
-  const pdf = await pdfjsLib.getDocument({ data }).promise;
-
-  let text = "";
-
-  for (let i = 1; i <= pdf.numPages; i++) {
-    const page = await pdf.getPage(i);
-    const content = await page.getTextContent();
-    text += content.items.map((item: any) => item.str).join(" ") + "\n";
-  }
-
-  return text;
+  const pdfParse = (await import("pdf-parse")).default as any;
+  
+  const data = await pdfParse(buffer);
+  
+  return data.text;
 };
