153dd3c5fb11db392ba02150c36c5438185aa616
Zeh-Eox <arnolddevmail@gmail.com>
2026-02-07 21:44:00 +0000

    Snowfall added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,24 +3,28 @@ import { Route, Routes } from "react-router-dom";
 import { ROUTES } from "./routes";
 import Layout from "./pages/Layout";
 import { Toaster } from "react-hot-toast";
+import Snowfall from "react-snowfall";
 
 const App: React.FunctionComponent = () => {
   return (
     <>
       <Toaster position="top-center" />
+
+      <Snowfall color="#5044E5" />
+
       <Routes>
         <Route path="/ai" element={<Layout />}>
           {ROUTES.filter((r) => r.isAiRoute).map(
             ({ route, component: Component }, i) => (
               <Route key={i} path={route} element={<Component />} />
-            )
+            ),
           )}
         </Route>
 
         {ROUTES.filter((r) => !r.isAiRoute).map(
           ({ route, component: Component }, i) => (
             <Route key={i} path={route} element={<Component />} />
-          )
+          ),
         )}
       </Routes>
     </>
